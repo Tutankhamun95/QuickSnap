cf418402d79c431c57217db786cc35bfa4da696d
Tutankhamun95 <ali-mahdy@windowslive.com>
2016-09-08 16:44:13 +0800

    Update Snap.cs

--- src/GameLogic/Snap.cs
+++ src/GameLogic/Snap.cs
@@ -114,6 +114,12 @@ namespace CardGames.GameLogic
 		public void Update()
 		{
 			//TODO: implement update to automatically slip cards!
+			
+			if (_gameTimer.Ticks > _flipTime)
+ 			{
+				 _gameTimer.Reset ();
+ 				 FlipNextCard ();
+ 			}
 		}
 
 		/// <summary>
